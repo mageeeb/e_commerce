bf424da24ec8319d2f546276ad4990d3ff38b7bc
Magib <112185392+mageeeb@users.noreply.github.com>
2023-12-07 11:04:45 +0100

    first_sov

--- assets/app.ts
+++
@@ -1,5 +0,0 @@
-import { registerReactControllerComponents } from '@symfony/ux-react';
-import './bootstrap.js';
-import './styles/app.css';
-
-registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
--- assets/utils.ts
+++
@@ -1,9 +0,0 @@
-export const formatPrice = (price: number) => {
-    const formatter = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' });
-
-    return formatter.format(price / 100);
-}
-
-export const visit = (url: string) => {
-    window.location.href = url
-}
